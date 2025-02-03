b5038a20b1b84a5b23475de1d759ae0032e8e121
Shimshimus <alyonashimanskaya528@gmail.com>
2025-02-03 15:20:53 +0200

    TC ci 01.09.02

--- tests/test_different_weather.py
+++ tests/test_different_weather.py
@@ -6,8 +6,9 @@ def test_different_weather_pop_up(welcome_page):
     welcome_page.open_different_weather_pop_up()
     expect(welcome_page.different_weather_pop_up).to_be_visible()
 
-def test_different_weather_send_button_is_clickable(welcome_page):
+def test_ci_different_weather_send_button_is_clickable(welcome_page):
     welcome_page.visit()
     welcome_page.open_different_weather_pop_up()
     welcome_page.choose_different_weather_first_option()
     welcome_page.different_weather_click_send_button()
+
