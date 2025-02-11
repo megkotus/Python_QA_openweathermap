580cdb287fdd11e86bf232bf23b6f7bced1090a2
Megkotus <megkotus@mail.ru>
2025-02-11 11:40:18 +0700

    'TC_01.07.01 | Remove unused imports'

---
+++ tests/test_date_and_time.py
@@ -0,0 +1,7 @@
+from playwright.sync_api import expect
+
+#TC 01.07.01 Check date and time | Compare displayed time with the current time at the displayed location
+def test_date_and_time(welcome_page):
+    welcome_page.visit()
+
+    expect(welcome_page.date_and_time).to_have_text(welcome_page.get_date_and_time_at_showed_location())
