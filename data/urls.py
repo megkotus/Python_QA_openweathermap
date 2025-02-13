ecc5bd83da02a32a2a67bf0fe3aa3587b7b27174
LubovLepekhina <lubov.lepekhina@gmail.com>
2025-02-13 19:25:29 +0100

    add api tests

--- data/urls.py
+++ data/urls.py
@@ -1,2 +1,3 @@
 class Urls:
     base_url = 'https://openweathermap.org/'
+    api_url = 'https://api.openweathermap.org/data/2.5/weather'
---
+++ tests/test_api.py
@@ -0,0 +1,21 @@
+import pytest
+from dotenv import load_dotenv
+import os
+import requests
+
+from data.urls import Urls
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+urls = Urls()
+
+@pytest.mark.parametrize('city', ['London', 'Podgorica', 'Berlin', 'Dubrovnik'])
+def test_weather_api_returns_correct_city_name(city):
+    response = requests.get(urls.api_url, params={'q': city, 'appid': api_key})
+    json_data = response.json()
+    assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
+    assert json_data['name'] == city, f"Expected {city}, but got {json_data['name']}"
+
+def test_api_key_is_required():
+    response = requests.get(urls.api_url, params={'q': 'Budva'})
+    assert response.status_code == 401
