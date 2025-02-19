af92129e6c09e1f5fe32343561367991987be0de
LubovLepekhina <113927670+LubovLepekhina@users.noreply.github.com>
2025-02-19 19:02:07 +0100

    API testing (#34)
    * added utils and refactored api-tests
    
    * fixed function get_weather_by_city_id and changed api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,23 +1,29 @@
 import pytest
-import os
-import requests
 
 from data.urls import Urls
+from utils.api_functions import get_weather_by_city_name, get_weather_by_zip_code, get_weather_by_city_id, \
+    get_cities_id_by_city_name
 
-if not os.getenv('CI'):
-    from dotenv import load_dotenv
-    load_dotenv()
-
-api_key = os.getenv('SECRET_KEY')
 urls = Urls()
 
 @pytest.mark.parametrize('city', ['London', 'Podgorica', 'Berlin', 'Dubrovnik'])
 def test_weather_api_returns_correct_city_name(city):
-    response = requests.get(urls.api_url, params={'q': city, 'appid': api_key})
+    response = get_weather_by_city_name(city)
     json_data = response.json()
     assert response.status_code == 200, f"Expected 200, but got {response.status_code}"
     assert json_data['name'] == city, f"Expected {city}, but got {json_data['name']}"
 
-def test_api_key_is_required():
-    response = requests.get(urls.api_url, params={'q': 'Budva'})
-    assert response.status_code == 401
+def test_get_weather_by_city_name_without_api_key():
+    response = get_weather_by_city_name('Budva', app_id = None)
+    assert response.status_code == 401, f'Expected 401, got {response.status_code}'
+
+def test_get_weather_by_zip_code_without_api_key():
+    response = get_weather_by_zip_code('191028', app_id = None)
+    assert response.status_code == 401, f'Expected 401, got {response.status_code}'
+
+@pytest.mark.parametrize('city', ['London', 'Podgorica', 'Berlin', 'Dubrovnik'])
+def test_weather_api_returns_correct_city_by_id(city):
+    response_city_id = get_cities_id_by_city_name(city)
+    response = get_weather_by_city_id(response_city_id[city])
+    assert response.status_code == 200, f'Expected 200, got {response.status_code}'
+    assert response.json()['name'] == city, f'Expected {city}, got {response.json()['name']}'
---
+++ utils/api_functions.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+from data.urls import Urls
+
+urls = Urls()
+
+if not os.getenv('CI'):
+    from dotenv import load_dotenv
+    load_dotenv()
+
+api_key = os.getenv('SECRET_KEY')
+
+def get_weather_by_city_name(city, app_id = api_key):
+    response = requests.get(urls.api_url, params={'q': city, 'appid': app_id})
+    return response
+
+def get_weather_by_zip_code(zip_code, app_id = api_key):
+    response = requests.get(urls.api_url, params={'q': zip_code, 'appid': app_id})
+    return response
+
+def get_weather_by_city_id(city_id, app_id = api_key):
+    response = requests.get(urls.api_url, params={'id': city_id, 'appid': app_id})
+    return response
+
+def get_cities_id_by_city_name(city):
+    response = get_weather_by_city_name(city)
+    data = response.json()
+    return {
+        city: data['id']
+    }
