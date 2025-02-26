fbedb0992f2c12149b49e2d82ab713ebae888aeb
Shimshimus <alyonashimanskaya528@gmail.com>
2025-02-26 11:15:25 +0200

    added Dashboard Page fixture

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 import pytest
 from playwright.sync_api import Page
+
+from pages.dashboard_page import DashboardPage
 from pages.welcome_page import WelcomePage
 
 @pytest.fixture
 def welcome_page(page:Page):
     return WelcomePage(page)
+
+@pytest.fixture
+def dashboard_page(page:Page):
+    return DashboardPage(page)
