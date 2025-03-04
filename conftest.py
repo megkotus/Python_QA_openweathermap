8c3b5ffe53ca5d9abe3505cffc7c9a8925f82f90
Shimshimus <alyonashimanskaya528@gmail.com>
2025-03-04 14:42:26 +0200

    I've formatted the code of the project with blue to make it look prettier

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Page
 from pages.welcome_page import WelcomePage
 
+
 @pytest.fixture
-def welcome_page(page:Page):
+def welcome_page(page: Page):
     return WelcomePage(page)
